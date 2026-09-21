ccfeba863aa00327ecc7d04309331eddd11a6b74
050418688 <q2016502@126.com>
2026-09-21 22:56:27 +0800

    v124: fix optional unwrap

--- MailTracker/plchaxun/EdgeSwipeBack.swift
+++ MailTracker/plchaxun/EdgeSwipeBack.swift
@@ -1,11 +1,10 @@
 import SwiftUI
 import UIKit
 
-// 全局启用边缘右滑返回
 extension UINavigationController: UIGestureRecognizerDelegate {
     open override func viewDidLoad() {
         super.viewDidLoad()
-        interactivePopGestureRecognizer.delegate = self
+        interactivePopGestureRecognizer?.delegate = self
     }
 
     public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
