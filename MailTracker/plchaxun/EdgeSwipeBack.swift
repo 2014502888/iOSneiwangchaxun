ed116fae9e515ee062602600eedba88f4b85655d
050418688 <q2016502@126.com>
2026-09-21 22:48:08 +0800

    v121: enable edge swipe back gesture both

---
+++ MailTracker/plchaxun/EdgeSwipeBack.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+import UIKit
+
+// 启用边缘右滑返回手势
+extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = nil
+    }
+}
