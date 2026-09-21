cdec5ecedf61fe3ed6dda091734b902652c2be68
050418688 <q2016502@126.com>
2026-09-21 15:40:01 +0800

    v91: use Color background onTapGesture

--- MailTracker/plchaxun/TapToDismiss.swift
+++ MailTracker/plchaxun/TapToDismiss.swift
@@ -1,28 +1,14 @@
 import SwiftUI
 import UIKit
 
-// 点击背景收键盘，不影响 TextEditor
 struct TapToDismissModifier: ViewModifier {
     func body(content: Content) -> some View {
-        content.background(TapToDismissView())
-    }
-}
-
-struct TapToDismissView: UIViewRepresentable {
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView()
-        view.backgroundColor = .clear
-        let tap = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.tap))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-        return view
-    }
-    func updateUIView(_ uiView: UIView, context: Context) {}
-    func makeCoordinator() -> Coordinator { Coordinator() }
-    class Coordinator: NSObject {
-        @objc func tap() {
-            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-        }
+        content.background(
+            Color(.systemBackground)
+                .onTapGesture {
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }
+        )
     }
 }
 
