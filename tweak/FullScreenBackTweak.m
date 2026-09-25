9cd76116a3e81599085343d8cc5b0dd4d4325706
050418688 <q2016502@126.com>
2026-09-25 12:29:21 +0800

    dylib: 恢复到能编译的基线版本

--- tweak/FullScreenBackTweak.m
+++ tweak/FullScreenBackTweak.m
@@ -1,26 +1,6 @@
-#import <UIKit/UIKit.h>
+﻿#import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 #import <objc/message.h>
-
-static NSString *kFBSKeyEnabled = @"fbs_enabled";
-static NSString *kFBSKeyHaptic  = @"fbs_haptic";
-static NSString *kFBSKeyStrength = @"fbs_strength";
-
-static BOOL FBSGetEnabled(void) {
-    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
-    id v = [d objectForKey:kFBSKeyEnabled];
-    return v ? [v boolValue] : YES;
-}
-static BOOL FBSGetHaptic(void) {
-    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
-    id v = [d objectForKey:kFBSKeyHaptic];
-    return v ? [v boolValue] : YES;
-}
-static double FBSGetStrength(void) {
-    NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
-    id v = [d objectForKey:kFBSKeyStrength];
-    return v ? [v doubleValue] : 0.8;
-}
 
 @interface FBSPanGesture : UIPanGestureRecognizer
 @end
@@ -42,7 +22,6 @@ static double FBSGetStrength(void) {
     return s;
 }
 - (void)track:(UIPanGestureRecognizer *)g {
-    if (!FBSGetHaptic()) return;
     CGFloat w = [UIScreen mainScreen].bounds.size.width;
     switch (g.state) {
         case UIGestureRecognizerStateBegan:
@@ -53,10 +32,7 @@ static double FBSGetStrength(void) {
             CGFloat tx = [g translationInView:g.view].x;
             CGFloat vx = [g velocityInView:g.view].x;
             if (tx > w * 0.35 || vx > 300) {
-                double s = FBSGetStrength();
-                if (s < 0.01) s = 0.01;
-                if (s > 1.0) s = 1.0;
-                [_gen impactOccurredWithIntensity:s];
+                [_gen impactOccurredWithIntensity:1.0];
             }
             _gen = nil;
             break;
@@ -93,7 +69,6 @@ static double FBSGetStrength(void) {
     return nil;
 }
 - (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)g {
-    if (!FBSGetEnabled()) return NO;
     if (![g isKindOfClass:[UIPanGestureRecognizer class]]) return NO;
     UIPanGestureRecognizer *p = (id)g;
     UINavigationController *nav = [self navOf:g.view];
@@ -105,99 +80,6 @@ static double FBSGetStrength(void) {
 }
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)g shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)o {
     return NO;
-}
-@end
-
-@interface FBSSettingsBall : UIView
-+ (instancetype)shared;
-- (void)show;
-@end
-@implementation FBSSettingsBall
-{
-    UIButton *_btn;
-}
-+ (instancetype)shared {
-    static FBSSettingsBall *s;
-    static dispatch_once_t t;
-    dispatch_once(&t, ^{ s = [FBSSettingsBall new]; });
-    return s;
-}
-- (instancetype)init {
-    CGRect f = [UIScreen mainScreen].bounds;
-    self = [super initWithFrame:CGRectMake(f.size.width - 56, f.size.height - 200, 44, 44)];
-    if (self) {
-        self.backgroundColor = [UIColor clearColor];
-        self.userInteractionEnabled = YES;
-
-        _btn = [UIButton buttonWithType:UIButtonTypeSystem];
-        _btn.frame = self.bounds;
-        _btn.layer.cornerRadius = 22;
-        _btn.clipsToBounds = YES;
-        _btn.backgroundColor = [UIColor colorWithRed:0.2 green:0.5 blue:1.0 alpha:0.85];
-        [_btn setTitle:@"设" forState:UIControlStateNormal];
-        [_btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        _btn.titleLabel.font = [UIFont boldSystemFontOfSize:16];
-        [_btn addTarget:self action:@selector(onTap) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:_btn];
-
-        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onPan:)];
-        [_btn addGestureRecognizer:pan];
-    }
-    return self;
-}
-- (void)show {
-    if (self.superview) return;
-    UIWindowScene *ws = nil;
-    for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
-        if ([s isKindOfClass:[UIWindowScene class]] && s.activationState == UISceneActivationStateForegroundActive) { ws = s; break; }
-    }
-    if (!ws) return;
-    for (UIWindow *w in ws.windows) {
-        if (w.isKeyWindow && w.rootViewController) {
-            [w addSubview:self];
-            return;
-        }
-    }
-}
-- (void)onPan:(UIPanGestureRecognizer *)p {
-    CGPoint tr = [p translationInView:self];
-    p.view.center = CGPointMake(p.view.center.x + tr.x, p.view.center.y + tr.y);
-    [p setTranslation:CGPointZero inView:self];
-}
-- (UIViewController *)topVC {
-    UIWindowScene *ws = nil;
-    for (UIWindowScene *s in [UIApplication sharedApplication].connectedScenes) {
-        if ([s isKindOfClass:[UIWindowScene class]] && s.activationState == UISceneActivationStateForegroundActive) { ws = s; break; }
-    }
-    if (!ws) return nil;
-    UIViewController *vc = ws.windows.firstObject.rootViewController;
-    while (vc.presentedViewController) vc = vc.presentedViewController;
-    return vc;
-}
-- (void)onTap {
-    UIViewController *top = [self topVC];
-    if (!top) return;
-    UIAlertController *ac = [UIAlertController alertControllerWithTitle:@"全屏返回设置" message:nil preferredStyle:UIAlertControllerStyleActionSheet];
-
-    BOOL en = FBSGetEnabled();
-    [ac addAction:[UIAlertAction actionWithTitle:en ? @"✓ 全屏返回: 开" : @"  全屏返回: 关" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){
-        [[NSUserDefaults standardUserDefaults] setBool:!en forKey:kFBSKeyEnabled];
-    }]];
-
-    BOOL ha = FBSGetHaptic();
-    [ac addAction:[UIAlertAction actionWithTitle:ha ? @"✓ 确认震动: 开" : @"  确认震动: 关" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){
-        [[NSUserDefaults standardUserDefaults] setBool:!ha forKey:kFBSKeyHaptic];
-    }]];
-
-    double st = FBSGetStrength();
-    [ac addAction:[UIAlertAction actionWithTitle:[NSString stringWithFormat:@"震动强度: %.0f%%", st*100] style:UIAlertActionStyleDefault handler:^(UIAlertAction *a){
-        double next = st + 0.25;
-        if (next > 1.0) next = 0.25;
-        [[NSUserDefaults standardUserDefaults] setDouble:next forKey:kFBSKeyStrength];
-    }]];
-
-    [ac addAction:[UIAlertAction actionWithTitle:@"关闭" style:UIAlertActionStyleCancel handler:nil]];
-    [top presentViewController:ac animated:YES completion:nil];
 }
 @end
 
@@ -237,7 +119,6 @@ static void FBSInstall(void) {
                 if (w.rootViewController) FBSWalk(w.rootViewController);
             }
         }
-        [[FBSSettingsBall shared] show];
     });
 }
 
